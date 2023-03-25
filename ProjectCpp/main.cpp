133bf3ba00b6b847207d10bbe8485dc40441b236
Displesher <Displesher@gmai.com>
2023-03-25 21:15:48 +0300

    first version

---
+++ ProjectCpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+	setlocale(LC_ALL, "RU");
+
+	cout << "Это программа на С++" << endl;
+
+	return 0;
+}
